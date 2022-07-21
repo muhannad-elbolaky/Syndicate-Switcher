de25f9937d7fa182e1ceb5144876e1c20157e28d
MajorAmari <muhannadexz@gmail.com>
2022-07-22 00:01:59 +0200

    change the build command

--- Build.bat
+++ Build.bat
@@ -4,4 +4,4 @@ set "output=-- Please install nodejs: https://nodejs.org/en/ --"
 for /f "delims=" %%i in ('node -v 2^>nul') do set output=%%i
 echo %output%
 echo.
-npm run build
+npm run build:script
